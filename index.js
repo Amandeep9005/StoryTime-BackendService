c166e0b587fb40d6bf1aa22bb2e4932490be12d0
Aman <Amandeepsinghkapoor@gmail.com>
2021-04-23 23:44:53 -0400

    Adding Server logic to emit any message it receives

--- index.js
+++ index.js
@@ -6,17 +6,10 @@ const io = require("socket.io")(http,{
     cors:{origin:"*"}
 });
 
-io.on("connection",(socket)=>{
-    console.log("a user connected");
-
-    socket.on("message",(message)=>{
-
-        console.log(message);
-
-        io.emit("message",`${socket.id.substr(0,2)} said ${message}`)
-    })
-
-
-})
+io.on("connection", (socket) => {
+    socket.on("new-operations", (data) => {
+      io.emit("new-remote-operations", data);
+    });
+  });
 
 http.listen(8080,()=> console.log(`listenting on http:\\localhost:8080`))
